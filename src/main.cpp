44191e5d66726d335e7d3690be47dd1dbca9ab23
chris <bigc123@hotmail.co.uk>
2021-10-22 11:44:45 +0100

    remove driver code

--- src/main.cpp
+++ src/main.cpp
@@ -8,27 +8,5 @@
 
 int main()
 {
-	//Singleton *newInstance = Singleton::getInstance();
-
-	/*Director director;
-	ConcreteProductBuilder builder;
-	director.makeConcreteProduct(&builder);
-	AbstractProduct *product = builder.build();
-	product->printState();*/
-
-	ConcreteFactory concreteFactory;
-	AbstractProduct *productA1 = concreteFactory.createProductA();
-	AbstractProduct *productB1 = concreteFactory.createProductB();
-
-	productA1->printState();
-	productB1->printState();
-
-	ManyThingsFactory manyThingsFactory;
-	AbstractProduct *productA2 = manyThingsFactory.createProductA();
-	AbstractProduct *productB2 = manyThingsFactory.createProductB();
-
-	productA2->printState();
-	productB2->printState();
-
 	return 0;
 }
